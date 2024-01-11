d9bc61b74d00de0ca1acf4f3ddc4fed76bdbe86a
Arqum01 <arqumahmad019@gmail.com>
2024-01-11 11:06:32 +0500

    Invoice Model

--- frontend/src/components/Admin/Invoice/Invoice.jsx
+++ frontend/src/components/Admin/Invoice/Invoice.jsx
@@ -53,34 +53,19 @@ const Invoice = () => {
 
   const tableItems = [
     {
-      name: "Liam James",
-      email: "liamjames@example.com",
-      position: "Software engineer",
-      salary: "$100K",
+      name: "Project Name",
+      SubName: "Home page / logo / Mockups ",
+      Price: "$7500",
     },
     {
-      name: "Olivia Emma",
-      email: "oliviaemma@example.com",
-      position: "Product designer",
-      salary: "$90K",
+      name: "Project Name",
+      SubName: "Home page / logo / Mockups ",
+      Price: "$7500",
     },
     {
-      name: "William Benjamin",
-      email: "william.benjamin@example.com",
-      position: "Front-end developer",
-      salary: "$80K",
-    },
-    {
-      name: "Henry Theodore",
-      email: "henrytheodore@example.com",
-      position: "Laravel engineer",
-      salary: "$120K",
-    },
-    {
-      name: "Amelia Elijah",
-      email: "amelia.elijah@example.com",
-      position: "Open source manager",
-      salary: "$75K",
+      name: "Project Name",
+      SubName: "Home page / logo / Mockups ",
+      Price: "$7500",
     },
   ];
 
@@ -243,101 +228,96 @@ const Invoice = () => {
 
       {/* ----------------- PREVIEW INVOICE ----------------- */}
       <Modal
-        icon={<FileText  size={28} color="#E92215" />}
-        size="6xl"
+        size="4xl"
         show={showModalX}
+        icon={<X size={30} onClick={onClickTwo} />}
       >
         <Modal.Header>
-          <div className="px-4 flex justify-between items-center border-b-4 border-gray-500 pb-2">
-            <p>
-              <span>Invoice:</span> 01/01/24
-            </p>
-            <p>
-              <span>Status:</span> Pending
-            </p>
-            {/* <button class="px-8 py-2.5 leading-5 font-light text-white transition-colors duration-300 transform bg-[#D22B2B] rounded-md hover:bg-[#D22B2B] focus:outline-none focus:bg-[#D22B2B]">
-              Download Invoice
-            </button> */}
-            <X size={32} onClick={onClickTwo} className="cursor-pointer hover:text-red-600" />
+          {/* <div className="flex justify-between items-center border-b-4 border-gray-500 pb-2"> */}
+          <div className="flex justify-between items-center">
+          <img
+                src="https://cdn.shopify.com/s/files/1/0704/6378/2946/files/ITEXPERTS_LOGO.png?v=1704170784"
+                alt="keep"
+                width="120"
+                height="40"
+                className=""
+              />
+            <p className="text-4xl text-black font-semibold modelHead me-14">Invoice</p>
           </div>
         </Modal.Header>
         <Modal.Body>
-          <div className="px-4 flex justify-between">
-            {/* ----------------- FROM ----------------- */}
-            <div className="p-5 w-100">
-              <p className="my-2 text-xl">From</p>
-              <h1 className="my-2 font-bold text-2xl">Webz Poland</h1>
-              <p className=" text-xl">United States of America</p>
-              <p className=" text-xl">71-123 Scotland,USA</p>
-              <p className=" text-xl">Email: IT@it-experts.com</p>
-              <p className=" text-xl">Phone: +48 222 444 666 33</p>
+          <div className=" flex justify-between">
+            <div className=" p-2 w-100">
+              <p className="modelClientText mb-2 text-base">Bill To:</p>
+              <h1 className="modelClientHeadText mb-2 font-semibold text-lg">Webz Poland</h1>
+              <p className="modelClientText mb-2 text-base">(+92) 334 41087865</p>
+              <p className="modelClientText mb-2 text-base">info@gmail.com</p>
             </div>
-
-            {/* ----------------- TO ----------------- */}
-            <div className="p-5 w-100">
-              <p className="my-2 text-xl">To</p>
-              <h1 className="my-2 font-bold text-2xl">Bob Anderson</h1>
-              <p className=" text-xl">United States of America</p>
-              <p className=" text-xl">71-123 Scotland,USA</p>
-              <p className=" text-xl">Email: client@client-email.com</p>
-              <p className=" text-xl">Phone: +48 222 444 666 33</p>
+            <div className=" p-2 w-100">
+              <p className="modelClientText mb-2 text-base">From:</p>
+              <h1 className="modelClientHeadText mb-2 font-semibold text-lg">Webz Poland</h1>
+              <p className="modelClientText mb-2 text-base">(+92) 334 41087865</p>
+              <p className="modelClientText mb-2 text-base">info@gmail.com</p>
+              <p className="modelClientText text-base">
+              3909 Heavner Avenue <br /> Mountain, GA 30083
+              </p>
             </div>
           </div>
-          <div className="mt-12 shadow-sm border rounded-lg overflow-x-auto">
-            {/* ----------------- TABLE ----------------- */}
+          <div className="flex justify-between mx-2 mt-5">
+            <p className="text-base">Reference No: 00004 </p>
+            <p className="text-base">04 December 2024 </p>
+          </div>
+          <div className="mt-5 shadow-sm border rounded-lg overflow-x-auto">
             <table className="w-full table-auto text-sm text-left">
-              <thead className="bg-gray-50 text-black font-bold text-xl border-b">
-                <tr className="divide-x">
-                  <th className="py-3 px-6">Service</th>
-                  <th className="py-3 px-6">Amount</th>
-                  <th className="py-3 px-6">dummy</th>
-                  <th className="py-3 px-6">Amount</th>
+              <thead className="bg-gray-200 text-black font-normal text-lg border-b">
+                <tr>
+                  <th className="py-3 px-6">Description</th>
+                  <th className="py-3 px-6 text-end">SubTotal</th>
                 </tr>
               </thead>
               <tbody className="text-gray-600 divide-y">
                 {tableItems.map((item, idx) => (
-                  <tr key={idx} className="divide-x">
-                    <td className="px-6 py-4 whitespace-nowrap flex items-center gap-x-6">
-                      {item.name}
+                  <tr key={idx}>
+                    <td className="ps-5 py-4 whitespace-nowrap flex items-end gap-x-6">
+                      {item.name} <br />
+                      {item.SubName}
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {item.email}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {item.position}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {item.salary}
+                    <td className="px-6 py-4 whitespace-nowrap text-end text-lg">
+                      {item.Price}
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
-          </div>
-          <div>
-            <table className="w-50 table-auto text-sm text-left flex justify-center mt-5 ">
-              <tbody className="text-gray-600 divide-y border-solid border-2 border-gray-200">
-                <tr className="divide-x">
-                  <td className="px-6 py-4 whitespace-nowrap flex items-center gap-x-6 text-2xl text-black font-bold">
-                    SubTotal
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-2xl">
-                    $ 8672
-                  </td>
+            <table className="w-full table-auto text-sm text-left">
+              <thead className="bg-gray-200 text-black text-lg border-b">
+                <tr>
+                  <th className="py-3 px-6 font-bold">Description</th>
+                  <th className="py-3 px-6 text-end">$150,000</th>
                 </tr>
-                <tr className="divide-x">
-                  <td className="ps-6 pe-40 py-4 whitespace-nowrap flex items-center gap-x-6 text-2xl text-black font-bold">
-                    Total
-                  </td>
-                  <td className="ps-6 pe-40 px-6 py-4 whitespace-nowrap text-2xl text-black font-bold">
-                    $ 8672
-                  </td>
-                </tr>
-              </tbody>
+              </thead>
             </table>
+          </div>
+          <div className="flex justify-center gap-5 mt-5">
+          <button
+              class="px-6 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-[#D22B2B] rounded-md hover:bg-[#D22B2B] focus:outline-none focus:bg-[#D22B2B]"
+              onClick={onClickTwo}
+              type="button"
+            >
+              Generate Invoice
+            </button>
+            <button
+              class="px-4 py-2.5 leading-5 text-black transition-colors duration-300 transform border-solid border-2 border-black bg-white rounded-md hover:bg-[#D22B2B] focus:outline-none "
+              onClick={onClickTwo}
+              type="button"
+            >
+              Download Invoice
+            </button>
           </div>
         </Modal.Body>
       </Modal>
+
+
     </>
   );
 };
